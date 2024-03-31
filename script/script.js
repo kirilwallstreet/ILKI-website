b4f6198bdd962cc16cbf4747d4e0a220f839fa21
kiril1234 <karat2007ll@gmail.com>
2024-03-31 22:22:44 +0200

    send email work!(&)

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
 var swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-      invert: false,
-    },
-    // autoHeight: true,
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
-    }
-  });
-
-  document.querySelector('.more-button').addEventListener('click', function () {
-    document.querySelector('.list-container').classList.toggle('active');
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
 });
 
+document.querySelector('.more-button').addEventListener('click', function () {
+  document.querySelector('.list-container').classList.toggle('active');
+});
+/*
 "use strict"
 
 document.addEventListener("DOMContentLoaded", function() { 
@@ -70,4 +70,16 @@ document.addEventListener("DOMContentLoaded", function() {
     return  !/^\w+([\.-]?\w+)*@\w+([\.-]P\w+)*(\.\w{2,8})+$/.test(input.value);
     
   }
-});
+});*/
+
+
+function sendMail(){
+  let parms = {
+      name : document.getElementById("formName").value,
+      email: document.getElementById("formEmail").value,
+      telegramname : document.getElementById("telegramname").value,
+      formMessage : document.getElementById("formMessage").value,
+  }
+
+  emailjs.send("service_01k0olt","template_myr3kyq",parms).then(alert("Email Send"))
+}
