f50e30077f5e55ed628b56ea450d73f1c9ee4e32
kiril1234 <karat2007ll@gmail.com>
2024-04-01 15:33:33 +0200

    email woks!!!

--- script/script.js
+++ script/script.js
@@ -15,62 +15,8 @@ var swiper = new Swiper('.blog-slider', {
 document.querySelector('.more-button').addEventListener('click', function () {
   document.querySelector('.list-container').classList.toggle('active');
 });
-/*
-"use strict"
 
-document.addEventListener("DOMContentLoaded", function() { 
-  const form =  document.getElementById('popup');
-  form.addEventListener('submit', formSend);
 
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll('._req');
-
-    for (let index = 0; index < array.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-      
-      if (input.classList.contains('_email')){
-          if (emailTest(input)){
-            formAddError(input);
-            error++;
-
-          }
-      }else if(input.getAttribute("type") === "checkbox" && input.checked === false){
-        formAddError(input);
-        error++
-      } else{
-        if (input.value === ''){
-          formAddError(input);
-          error++;
-        }
-      }
-    }
-  }
-
-  function formAddError(input) {
-    input.parentElement.classList.add('_error');
-    input.classList.add('_error');
-
-  }
-
-  function formRemoveError(input) {
-    input.parentElement.classList.remove('_error');
-    input.classList.remove('_error');
-
-  }
-
-  function emailTest(input) {
-    return  !/^\w+([\.-]?\w+)*@\w+([\.-]P\w+)*(\.\w{2,8})+$/.test(input.value);
-    
-  }
-});*/
 
 
 function sendMail(){
@@ -79,7 +25,10 @@ function sendMail(){
       email: document.getElementById("formEmail").value,
       telegramname : document.getElementById("telegramname").value,
       formMessage : document.getElementById("formMessage").value,
+      select : document.getElementById("popup__select").value,
   }
+
+
 
   emailjs.send("service_01k0olt","template_myr3kyq",parms).then(alert("Email Send"))
 }
